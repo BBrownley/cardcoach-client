bc82086908aac086aa7c5e19b2394145a3a85fcf
BBrownley <brendobrownley@yahoo.com>
2023-05-02 02:15:26 -0400

    created separate flashcard Set component for displaying flashcard Sets in user's dashboard

--- src/components/views/Dashboard/Dashboard.jsx
+++ src/components/views/Dashboard/Dashboard.jsx
@@ -7,211 +7,126 @@ import { faStar, faPlusCircle } from "@fortawesome/free-solid-svg-icons";
 
 import ProgressBar from "@ramonak/react-progress-bar";
 
+import Set from "../Set/Set";
+
+const sets = [
+  {
+    title: "Introduction to JavaScript",
+    description:
+      "This class covers the basics of JavaScript programming, including variables, data types, functions, and control structures.",
+    totalTerms: 100,
+    mastered: 30
+  },
+  {
+    title: "Advanced JavaScript",
+    description:
+      "This class covers more advanced topics in JavaScript programming, including object-oriented programming, regular expressions, and error handling.",
+    totalTerms: 150,
+    mastered: 60
+  },
+  {
+    title: "HTML and CSS",
+    description:
+      "This class covers the basics of HTML and CSS, including elements, attributes, selectors, and styling.",
+    totalTerms: 120,
+    mastered: 40
+  },
+  {
+    title: "Responsive Web Design",
+    description:
+      "This class covers the principles of responsive web design, including fluid layouts, flexible media, and breakpoints.",
+    totalTerms: 80,
+    mastered: 20
+  },
+  {
+    title: "Node.js",
+    description:
+      "This class covers the basics of Node.js, including event-driven programming, asynchronous I/O, and the Node.js runtime environment.",
+    totalTerms: 90,
+    mastered: 30
+  },
+  {
+    title: "Express.js",
+    description:
+      "This class covers the basics of Express.js, including routing, middleware, and templates.",
+    totalTerms: 110,
+    mastered: 50
+  },
+  {
+    title: "React",
+    description:
+      "This class covers the basics of React, including components, props, and state.",
+    totalTerms: 100,
+    mastered: 40
+  },
+  {
+    title: "React Native",
+    description:
+      "This class covers the basics of React Native, including components, layouts, and the React Native API.",
+    totalTerms: 120,
+    mastered: 60
+  },
+  {
+    title: "Vue.js",
+    description:
+      "This class covers the basics of Vue.js, including components, templates, and directives.",
+    totalTerms: 100,
+    mastered: 30
+  },
+  {
+    title: "Angular",
+    description:
+      "This class covers the basics of Angular, including components, templates, and services.",
+    totalTerms: 110,
+    mastered: 40
+  },
+  {
+    title: "Python Fundamentals",
+    description:
+      "This class covers the basics of Python programming, including variables, data types, control structures, and functions.",
+    totalTerms: 150,
+    mastered: 50
+  },
+  {
+    title: "Python Data Science",
+    description:
+      "This class covers the basics of data science in Python, including NumPy, Pandas, and Matplotlib.",
+    totalTerms: 120,
+    mastered: 60
+  },
+  {
+    title: "Machine Learning with Python",
+    description:
+      "This class covers the basics of machine learning in Python, including supervised learning, unsupervised learning, and neural networks.",
+    totalTerms: 200,
+    mastered: 80
+  },
+  {
+    title: "Java Fundamentals",
+    description:
+      "This class covers the basics of Java programming, including variables, data types, control structures, and classes.",
+    totalTerms: 150,
+    mastered: 60
+  },
+  {
+    title: "Java Web Development",
+    description:
+      "This class covers the basics of web development in Java, including servlets, JSP, and Spring.",
+    totalTerms: 120,
+    mastered: 40
+  }
+];
+
 export default function Dashboard() {
   return (
     <Container>
       <Wrapper>
         <h4>Recently studied</h4>
         <div className="recent-sets set-container">
-          <div className="set">
-            <div className="set__title-desc">
-              <div className="title">
-                Computer Architecture and Operating Systems Management
-              </div>
-              <div className="desc">
-                Terms and definitions for C952 and C191
-              </div>
-            </div>
-
-            <div className="set__term-count">47 terms</div>
-
-            <div className="progress">
-              {" "}
-              <FontAwesomeIcon icon={faStar} className="progress__star" />
-              <ProgressBar
-                completed={22}
-                bgColor="#D9D9D9"
-                height="1.25rem"
-                baseBgColor="#777777"
-                labelColor="#6e7faa"
-                maxCompleted={47}
-                customLabel={22}
-                className="progress__bar"
-              />
-            </div>
-          </div>
+          <Set set={sets[0]} />
+          <Set set={sets[1]} />
         </div>
-        <h4>Your sets</h4>
-        <div className="user-sets set-container">
-          <div className="set">
-            <div className="set__title-desc">
-              <div className="title">
-                Computer Architecture and Operating Systems Management
-              </div>
-              <div className="desc">
-                Terms and definitions for C952 and C191
-              </div>
-            </div>
 
-            <div className="set__term-count">47 terms</div>
-
-            <div className="progress">
-              {" "}
-              <FontAwesomeIcon icon={faStar} className="progress__star" />
-              <ProgressBar
-                completed={22}
-                bgColor="#D9D9D9"
-                height="1.25rem"
-                baseBgColor="#777777"
-                labelColor="#6e7faa"
-                maxCompleted={47}
-                customLabel={22}
-                className="progress__bar"
-              />
-            </div>
-          </div>
-          <div className="set">
-            <div className="set__title-desc">
-              <div className="title">
-                Computer Architecture and Operating Systems Management
-              </div>
-              <div className="desc">
-                Terms and definitions for C952 and C191
-              </div>
-            </div>
-
-            <div className="set__term-count">47 terms</div>
-
-            <div className="progress">
-              {" "}
-              <FontAwesomeIcon icon={faStar} className="progress__star" />
-              <ProgressBar
-                completed={22}
-                bgColor="#D9D9D9"
-                height="1.25rem"
-                baseBgColor="#777777"
-                labelColor="#6e7faa"
-                maxCompleted={47}
-                customLabel={22}
-                className="progress__bar"
-              />
-            </div>
-          </div>
-          <div className="set">
-            <div className="set__title-desc">
-              <div className="title">
-                Computer Architecture and Operating Systems Management
-              </div>
-              <div className="desc">
-                Terms and definitions for C952 and C191
-              </div>
-            </div>
-
-            <div className="set__term-count">47 terms</div>
-
-            <div className="progress">
-              {" "}
-              <FontAwesomeIcon icon={faStar} className="progress__star" />
-              <ProgressBar
-                completed={22}
-                bgColor="#D9D9D9"
-                height="1.25rem"
-                baseBgColor="#777777"
-                labelColor="#6e7faa"
-                maxCompleted={47}
-                customLabel={22}
-                className="progress__bar"
-              />
-            </div>
-          </div>
-          <div className="set">
-            <div className="set__title-desc">
-              <div className="title">
-                Computer Architecture and Operating Systems Management
-              </div>
-              <div className="desc">
-                Terms and definitions for C952 and C191
-              </div>
-            </div>
-
-            <div className="set__term-count">47 terms</div>
-
-            <div className="progress">
-              {" "}
-              <FontAwesomeIcon icon={faStar} className="progress__star" />
-              <ProgressBar
-                completed={22}
-                bgColor="#D9D9D9"
-                height="1.25rem"
-                baseBgColor="#777777"
-                labelColor="#6e7faa"
-                maxCompleted={47}
-                customLabel={22}
-                className="progress__bar"
-              />
-            </div>
-          </div>
-          <div className="set">
-            <div className="set__title-desc">
-              <div className="title">
-                Computer Architecture and Operating Systems Management
-              </div>
-              <div className="desc">
-                Terms and definitions for C952 and C191
-              </div>
-            </div>
-
-            <div className="set__term-count">47 terms</div>
-
-            <div className="progress">
-              {" "}
-              <FontAwesomeIcon icon={faStar} className="progress__star" />
-              <ProgressBar
-                completed={22}
-                bgColor="#D9D9D9"
-                height="1.25rem"
-                baseBgColor="#777777"
-                labelColor="#6e7faa"
-                maxCompleted={47}
-                customLabel={22}
-                className="progress__bar"
-              />
-            </div>
-          </div>
-        </div>
-        <h4>Shared with you</h4>
-        <div className="shared-sets set-container">
-          <div className="set">
-            <div className="set__title-desc">
-              <div className="title">
-                Computer Architecture and Operating Systems Management
-              </div>
-              <div className="desc">
-                Terms and definitions for C952 and C191
-              </div>
-            </div>
-
-            <div className="set__term-count">47 terms</div>
-
-            <div className="progress">
-              {" "}
-              <FontAwesomeIcon
-                icon={faStar}
-                className="progress__star full-mastery"
-              />
-              <ProgressBar
-                completed={47}
-                bgColor="#AEAA68"
-                height="1.25rem"
-                baseBgColor="#777777"
-                labelColor="#000000"
-                maxCompleted={47}
-                customLabel={"Full mastery!"}
-                className="progress__bar"
-              />
-            </div>
-          </div>
-        </div>
         <div className="create-set-button">
           <FontAwesomeIcon icon={faPlusCircle} className="plus-circle" />
           <h4>Create new set</h4>
---
+++ src/components/views/Set/Set.jsx
@@ -0,0 +1,47 @@
+/* 
+  The Set component represents an element in the user's dashboard. More specifically, it represents a flashcard Set. Users will be able to 
+  display information about each Set, including its title, # of terms,
+  and overall mastery progress
+*/
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar, faPlusCircle } from "@fortawesome/free-solid-svg-icons";
+
+import ProgressBar from "@ramonak/react-progress-bar";
+
+import React from "react";
+
+import { Container } from "./Set.elements";
+
+export default function Set(props) {
+  const { title, description, totalTerms, mastered } = props.set;
+
+  // goes into the flashcard set for user to begin studying from it
+  const goToSet = () => {
+    console.log("opening set");
+  };
+
+  return (
+    <Container onClick={goToSet}>
+      <div className="title-desc">
+        <div className="title">{title}</div>
+        <div className="desc">{description}</div>
+      </div>
+      <div className="term-count">{totalTerms} terms</div>
+      <div className="progress">
+        {" "}
+        <FontAwesomeIcon icon={faStar} className="progress__star" />
+        <ProgressBar
+          completed={mastered}
+          bgColor="#D9D9D9"
+          height="1.25rem"
+          baseBgColor="#777777"
+          labelColor="#6e7faa"
+          maxCompleted={totalTerms}
+          customLabel={mastered}
+          className="progress__bar"
+        />
+      </div>
+    </Container>
+  );
+}
