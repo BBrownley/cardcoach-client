4cf0fc45f49cbb0fe220d38eb3e919f6598cb821
BBrownley <brendobrownley@yahoo.com>
2023-05-28 03:12:15 -0400

    added state for card sets and their individual cards when creating new sets

--- src/components/views/CreateSet/SetCards/Card/Card.jsx
+++ src/components/views/CreateSet/SetCards/Card/Card.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrash, faStar } from "@fortawesome/free-solid-svg-icons";
@@ -6,14 +6,39 @@ import { faTrash, faStar } from "@fortawesome/free-solid-svg-icons";
 import { Container } from "./Card.elements";
 
 export default function Card(props) {
-  const { order } = props;
+  const { id, term, definition, updateTermInSet, updateDefInSet } = props;
+
+  const [cardTerm, setCardTerm] = useState(term);
+  const [cardDefinition, setCardDefinition] = useState(definition);
+
+  /**
+   * Updates the state of this Card component along with its parent SetCards to reflect the change
+   * @param updatedTerm - The updated term
+   */
+  const handleTermChange = updatedTerm => {
+    // update current child state
+    setCardTerm(updatedTerm);
+
+    // update parent state (set of all cards)
+    updateTermInSet(id, updatedTerm);
+  };
+
+  const handleDefinitionChange = updatedDefinition => {
+    // update current child state
+    setCardDefinition(updatedDefinition);
+
+    // update parent state (set of all cards)
+    updateDefInSet(id, updatedDefinition);
+  };
 
   return (
-    <Container id={props.order}>
+    <Container id={id}>
       <div className="card-top">
-        <div className="card-top__order">{order}</div>
+        <div className="card-top__order">{id}</div>
         <div className="card-top__right">
-          <div className="mastery-level">2/10 <FontAwesomeIcon icon={faStar} className="star-icon" /></div>
+          <div className="mastery-level">
+            2/10 <FontAwesomeIcon icon={faStar} className="star-icon" />
+          </div>
           <div className="delete">
             <FontAwesomeIcon icon={faTrash} className="delete-icon" />
           </div>
@@ -21,15 +46,26 @@ export default function Card(props) {
       </div>
       <div className="card-main">
         <div className="card-main__term">
-          <label htmlFor={`term${order}`}>Term</label>
-          <input type="text" id={`term${order}`} />
+          <label htmlFor={`term${id}`}>Term</label>
+          <input
+            type="text"
+            id={`term${id}`}
+            value={cardTerm}
+            onChange={e => handleTermChange(e.target.value)}
+          />
         </div>
 
         <div className="card-main__definition">
-          <label htmlFor={`definition${order}`} className="label">
+          <label htmlFor={`definition${id}`} className="label">
             Definition
           </label>
-          <textarea id={`definition${order}`} cols="30" rows="10"></textarea>
+          <textarea
+            id={`definition${id}`}
+            cols="30"
+            rows="10"
+            value={cardDefinition}
+            onChange={e => handleDefinitionChange(e.target.value)}
+          ></textarea>
         </div>
       </div>
     </Container>
--- src/components/views/CreateSet/SetCards/SetCards.jsx
+++ src/components/views/CreateSet/SetCards/SetCards.jsx
@@ -1,13 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Card from "./Card/Card";
 
 export default function SetCards() {
+  const [cards, setCards] = useState([
+    { id: 1, term: "termA", definition: "defA" },
+    { id: 2, term: "termB", definition: "defB" },
+    { id: 3, term: "termC", definition: "defC" }
+  ]);
+
+  /**
+   * Updates the state of the cards in the set. Uses the ID passed in as a param
+   * to determine which card needs to be updated
+   */
+  const handleSetUpdate = (updateID, updatedTerm) => {
+    setCards(
+      cards.map(card =>
+        card.id === updateID ? { ...card, term: updatedTerm } : card
+      )
+    );
+  };
+
+  const handleDefinitionUpdate = (updateID, updatedDefinition) => {
+    setCards(
+      cards.map(card =>
+        card.id === updateID ? { ...card, definition: updatedDefinition } : card
+      )
+    );
+  };
+
+  const printSet = () => {
+    console.log(cards);
+  };
+
   return (
     <div>
-      <Card order={1} />
-      <Card order={2} />
-      <Card order={3} />
+      {cards.map((card, index) => {
+        return (
+          <Card
+            id={index + 1}
+            updateTermInSet={handleSetUpdate}
+            updateDefInSet={handleDefinitionUpdate}
+            term={card.term}
+            definition={card.definition}
+          />
+        );
+      })}
+
+      <div onClick={printSet}>print set</div>
     </div>
   );
 }
